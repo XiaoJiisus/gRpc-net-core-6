13fb9c6ca1c25a065a544283d86dbd462c2b7117
Ivan Vazquez <xiao-jiisus@outlook.com>
2023-03-21 15:57:18 -0600

    First chapter ended

--- Chapter1/CountryReplyMapper.cs
+++ Chapter1/CountryReplyMapper.cs
@@ -1,12 +1,26 @@
+using Chapter1.Models;
+
 namespace Chapter1.Mappers;
 
 public static class CountryReplyMapper
 {
-    // public static CountryReply ToReply(this CountryModel country)
-    // {
-    //     if(country is null)
-    //         return null;
+    public static CountryReply ToReply(this CountryModel country)
+    {
+        if(country is null)
+            return null;
         
-    //     var countryReply = new CountryReply
-    // }
+        var countryReply = new CountryReply
+        {
+            Id = country.Id,
+            Name = country.Name,
+            Description = country.Description,
+            Anthem = country.Anthem,
+            CapitalCity = country.CapitalCity,
+            FlagURI = country.FlagURI
+        };
+
+        countryReply.Languages.AddRange(country.Languages);
+
+        return countryReply;
+    }
 }
---
+++ Chapter1/Models/CountryModel.cs
@@ -0,0 +1,12 @@
+namespace Chapter1.Models;
+
+public class CountryModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Anthem { get; set; } = string.Empty;
+    public string CapitalCity { get; set; } = string.Empty;
+    public string FlagURI { get; set; } = string.Empty;
+    public List<string> Languages { get; set; }
+}
